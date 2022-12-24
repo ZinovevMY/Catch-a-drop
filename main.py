b606abcc7d2ec4f4c97238c203ebdce958a14961
Mikhail <mehan@list.ru>
2022-12-24 18:55:49 +0300

    Add pictures and main window

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
+import tkinter as tk
 
+
+root = tk.Tk()
+root.title("Das is QUAKE!")
+root.geometry("900x450+300+250")
+root.resizable(width=False, height=False)
+
+bg_image = tk.PhotoImage(file='background.png')
+icon = tk.PhotoImage(file='rifle.png')
+
+root.iconphoto(False, icon)
+
+label1 = tk.Label(root, image=bg_image)
+label1.place(x=0, y=0)
+
+root.mainloop()
