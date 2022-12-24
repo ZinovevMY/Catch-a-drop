f680d96e0d2a98171b0a8a2e5b9ce27e0d3e7b47
Mikhail <mehan@list.ru>
2022-12-24 19:01:36 +0300

    refaktoring

--- main.py
+++ main.py
@@ -1,17 +1,4 @@
-import tkinter as tk
+import app_window as aw
 
-
-root = tk.Tk()
-root.title("Das is QUAKE!")
-root.geometry("900x450+300+250")
-root.resizable(width=False, height=False)
-
-bg_image = tk.PhotoImage(file='background.png')
-icon = tk.PhotoImage(file='rifle.png')
-
-root.iconphoto(False, icon)
-
-label1 = tk.Label(root, image=bg_image)
-label1.place(x=0, y=0)
-
-root.mainloop()
+if __name__ == '__main__':
+    aw.main_window()
